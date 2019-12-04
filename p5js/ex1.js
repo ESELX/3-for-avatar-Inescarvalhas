a28e903ee1b955f602645cec2993f24c50938305
Inescarvalhas <48055549+Inescarvalhas@users.noreply.github.com>
2019-12-04 15:45:11 +0000

    Avatar 2

--- p5js/ex1.js
+++ p5js/ex1.js
@@ -14,7 +14,60 @@ function setup() { //código que só corre uma vez
 }
 
 function draw() { //código que corre em loop
-  background(220); //podem apagar esta linha!
+createCanvas(400, 400);
+}
 
+function draw() {
+  background(220);
+}
+
+let numDiv;
+
+function setup() {
+  createCanvas(400, 400);
+}
+function draw() {
+  background(212, 244, 254);
+  numDiv = 8;//map(mouseX,0,width,5,50);
+
+  for (i = 0; i < numDiv-1; i++){
+    for (j = 0; j <numDiv-1; j++){
+      avatar((i+1)*width/numDiv,(j+1)*height/numDiv, 15);
+
+    }
+  }
+}
+
+function avatar(posX, posY, diam){
+
+  fill (252, 242, 123)
+  ellipse(posX-diam/1.2,posY-diam/1.2,diam/1.2);
+  fill (252, 242, 123)
+  ellipse(posX+diam/1.5,posY-diam/1.2,diam/1.2);
+  fill (251, 229, 218 )
+  ellipse(posX-diam/1.2,posY-diam/1.2,diam/1.9);
+  fill (251, 229, 218 )
+  ellipse(posX+diam/1.5,posY-diam/1.2,diam/1.9);
+
+
+  fill(252, 242, 123)
+  ellipse(posX,posY,diam*2);
+
+
+  fill (0)
+  ellipse(posX+diam*0,posY+diam/5,diam/2.5);
+
+  fill (251, 203, 179 )
+  ellipse(posX+diam*0,posY+diam/1.5,diam/3);
+
+  fill (250)
+  ellipse(posX-diam/1.9,posY-diam/5,diam/1.5);
+  ellipse(posX+diam/1.9,posY-diam/5,diam/1.5);
+  fill (0)
+  ellipse(posX-diam/1.9,posY-diam/5,diam/2.5);
+  ellipse(posX+diam/1.9,posY-diam/5,diam/2.5);
+  fill (250)
+  ellipse(posX-diam/1.6,posY-diam/3,diam/5);
+  ellipse(posX+diam/2.2,posY-diam/3,diam/5);
 
 }
